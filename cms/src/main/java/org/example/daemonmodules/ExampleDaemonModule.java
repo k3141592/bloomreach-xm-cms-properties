39ea15a6d6e581efd2d448b02206889f83cb7875
Koen v/d Weijden <koen.vanderweijden@bloomreach.com>
2021-02-05 23:36:34 +0100

    Added properties file

--- cms/src/main/java/org/example/daemonmodules/ExampleDaemonModule.java
+++ cms/src/main/java/org/example/daemonmodules/ExampleDaemonModule.java
@@ -6,12 +6,31 @@ import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class ExampleDaemonModule implements DaemonModule {
+    // Java system property that defines the path to the application properties file.
+    private static final String APPLICATION_PROPERTIES_FILE="application.properties";
+
     private static Logger log = LoggerFactory.getLogger(ExampleDaemonModule.class);
+
+    private Properties appProperties = new Properties();
+
+    public ExampleDaemonModule() {
+        String appPropertiesFile = System.getProperty(APPLICATION_PROPERTIES_FILE);
+        try {
+            appProperties.load(new FileInputStream(appPropertiesFile));
+        } catch (IOException e) {
+            log.error("Not able to load properties from property file: {} Error: {}", appPropertiesFile, e.getMessage());
+        }
+    }
 
     @Override
     public void initialize(Session session) throws RepositoryException {
+        // Print all properties
+        appProperties.stringPropertyNames().forEach(key -> log.info("Key {} - Value {}", key, appProperties.getProperty(key)));
         log.info("Module initialized");
     }
 
